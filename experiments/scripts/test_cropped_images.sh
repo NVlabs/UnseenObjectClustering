cb5dc131882fa5c6bac8e5b5391d1642dcc29067
Yu Xiang <yuxiang@cs260572.utdallas.edu>
2022-10-06 22:04:05 -0500

    use rgbd for cropped testing

--- experiments/scripts/test_cropped_images.sh
+++ experiments/scripts/test_cropped_images.sh
@@ -6,8 +6,9 @@ export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
 ./tools/test_images.py \
-  --imgdir data/hand-object/original_depth_images/ \
+  --imgdir data/hand-object/ \
+  --color color_*.jpg \
   --depth depth_*.png \
   --network seg_resnet34_8s_embedding \
-  --pretrained data/checkpoints/seg_resnet34_8s_embedding_cosine_depth_crop_sampling_epoch_16.checkpoint.pth \
-  --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_depth_tabletop.yml
+  --pretrained data/checkpoints/seg_resnet34_8s_embedding_cosine_rgbd_add_crop_sampling_epoch_16.checkpoint.pth \
+  --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_rgbd_add_tabletop.yml
